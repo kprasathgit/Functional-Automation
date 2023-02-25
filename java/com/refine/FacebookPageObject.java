325f303d667e0905b6738d69522b3fcb039e8ddb
kprasathgit <126261107+kprasathgit@users.noreply.github.com>
2023-02-25 09:44:47 +0530

    Add files via upload

---
+++ java/com/refine/FacebookPageObject.java
@@ -0,0 +1,39 @@
+package com.refine;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FacebookPageObject {
+
+	WebDriver driver;
+
+	public FacebookPageObject(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver = driver;
+	}
+	//this
+	By fbusername = By.cssSelector("#email");
+	By fbpassword = By.xpath("//input[@id='pass']");
+	By fblogin = By.cssSelector("button[name='login']");
+	By fbforgottenpassword = By.linkText("Forgotten password?");
+
+	public WebElement getUsername() {
+		return driver.findElement(fbusername);
+	}
+
+	public WebElement getPassWord() {
+		return driver.findElement(fbpassword);
+
+	}
+
+	public WebElement getLoginButton() {
+		return driver.findElement(fblogin);
+
+	}
+
+	public WebElement getForottonPassword() {
+		return driver.findElement(fbforgottenpassword);
+
+	}
+}
